66311461b0946f151406176aefa9a9243053a520
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-05 14:01:17 +0300

    processes

---
+++ app/controllers/Processes.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Processes extends Controller{
+
+  def list(modelId: Int) {
+    NotImplemented
+  }
+  
+  def details(id: Int) {
+    NotImplemented
+  }
+  
+  def delete(id: Int) {
+    NotImplemented
+  }
+}
