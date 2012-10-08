91d66ce532e89de8e765a27cc6c79b8718478cc8
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-08 18:59:57 +0300

    json

---
+++ app/controllers/ControllerJSON.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.libs.Json.toJson
+import models._
+import play.api._
+import play.api.mvc._
+import com.codahale.jerkson.Json._
+
+object ControllerJSON extends Controller {
+
+  def showElement = Action { implicit request =>
+    val json = generate(
+      Map(
+        "url" -> "http://nytimes.com",
+        "attributes" -> Map(
+          "name" -> "nytimes",
+          "country" -> "US",
+          "id" -> 25),
+        "links" -> List(
+          "http://link1",
+          "http://link2")))
+   Ok("")//views.html.showJson(json))
+  }
+}
