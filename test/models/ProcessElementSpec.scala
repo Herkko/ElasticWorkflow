259b8d4967f0d10e67f2b71c183e63390b17c92a
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-25 20:04:46 +0300

    more tests 2

--- test/models/ProcessElementSpec.scala
+++ test/models/ProcessElementSpec.scala
@@ -32,7 +32,7 @@ class ProcessElementSpec extends Specification {
           SQL("""insert into models values (1, 'ModelName1', '2000-10-11')""").execute()
           SQL("""insert into processes values (2, 'ProcessName1', '2000-10-11')""").execute()
           SQL("""insert into modelProcesses values (1, 1, 2)""").execute()
-          
+
           SQL("""insert into processElements values (1, 1, 3, 'ModelName1', 10, 200, 200)""").execute()
           SQL("""insert into processElements values (1, 2, 4, 'ModelName2', 10, 100, 100)""").execute()
         }
@@ -52,12 +52,36 @@ class ProcessElementSpec extends Specification {
         Process.create(Id(2), "ProcessName", new Date())
         ModelProcess.create(Id(3), 1, 2)
         ProcessElement.create(3, 1, Id(4), "I am Process Element", 10, 99, 99)
-        
+
         val result = ProcessElement.read(4)
         result.relationId.toString().toInt must equalTo(4)
         result.value must equalTo("I am Process Element")
       }
     }
-    
+  }
+
+  "Start Element" >> {
+    "exists" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        DB.withConnection {
+          implicit connection =>
+            val result = SQL("select * from elementTypes where name = 'Start'").execute()
+            result must beTrue
+        }
+      }
+    }
+
+    "can be created and retrieve by id" in {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+        Model.create(Id(1), "Name1", new Date())
+        Process.create(Id(2), "ProcessName", new Date())
+        ModelProcess.create(Id(3), 1, 2)
+        ProcessElement.create(3, 2, Id(4), "I am Start Element", 10, 99, 99)
+
+        val result = ProcessElement.read(4)
+        result.relationId.toString().toInt must equalTo(4)
+        result.value must equalTo("I am Start Element")
+      }
+    }
   }
 }
