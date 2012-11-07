03cb9bec892526a8ff86bef9213029e85b26ec5f
Panu Klemola <panu.klemola@gmail.com>
2012-11-07 13:29:12 +0200

    siistitty updateall()

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -25,29 +25,11 @@ window.onload = function() {
 
 
 function updateall(){
-    //var uusi = new activity({"modelProcessId":1,"elementTypeId":4,"relationId":4,"value":"Activity","size":0,"cx":170,"cy":110});
-	//var uusi = new activity({cx: 500, cy: 250, id: 4});
-    //var uusi = ActivityElements.at(1);
-    
-    //var testjson = uusi.toJSON();
-    
-    //alert(testjson);
-	//uusi.save();
+	for (var i=0; i<ActivityElements.length; i++) {
+		//TEE JOTAIN!
+	}
     ActivityElements.at(1).save();
     alert("testi");
-	/*
-	for (var i=0; i<ActivityElements.length; i++){
-		var testi = ActivityElements.pop(i);
-		var testinSisaltamaRaphaelElementti = testi.get("element");
-		var xArvo = testinSisaltamaRaphaelElementti.attr("x");
-		var yArvo = testinSisaltamaRaphaelElementti.attr("y");
-		//testi.set({cx: xArvo, cy: yArvo});
-		//alert("x " +xArvo+ " y " +yArvo+ " cx " + testi.get("cx") + " cy " + testi.get("cy"))
-		//testi.collection = ActivityElements;
-		testi.save({cx: xArvo, cy: yArvo});
-		alert("ei vittu toimi!");
-	}
-   	*/
     
     
 } 
