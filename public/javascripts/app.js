9a9f8b02c9f8d2b532da167a3ee3270cee119018
Daria Antonova <daria.antonova@helsinki.fi>
2012-11-14 14:11:26 +0200

    tekstin sboousgfho

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -45,3 +45,17 @@ function updateall(){
 	
     alert("Elements hopefully saved, now refresh page.");
 } 
+
+function changeText(id, text) {
+	var elem = getBackboneModelById(2);
+	elem.set({value: "Rawr2"});
+	elem.save();
+}
+
+function getBackboneModelById(id) {
+	    if(ActivityElements.get(id) != null) return ActivityElements.get(id);
+	    else if(StartElements.get(id) != null) return StartElements.get(id);
+	    else if(EndElements.get(id) != null) return EndElements.get(id);
+	    else if(SwimlaneElements.get(id) != null) return SwimlaneElements.get(id);
+	    else if(GatewayElements.get(id) != null) return GatewayElements.get(id);
+}
