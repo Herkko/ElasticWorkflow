13331652436720edceb50691535ed1cb4c862b81
Panu Klemola <panu.klemola@gmail.com>
2012-12-02 18:30:20 +0200

    noticet lisätty

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,25 @@
+/*Copyright 2012 University of Helsinki, Panu Klemola, Herkko Virolainen, Daria Antonova
+*
+*Licensed under the Apache License, Version 2.0 (the "License");
+*you may not use this file except in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing, software
+*distributed under the License is distributed on an "AS IS" BASIS,
+*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*See the License for the specific language governing permissions and
+*limitations under the License.*/
 
 var workflow = {
     models: {},
     collections: {},
     views: {},
 
-  //  domainHost: "http://morning-fjord-4117.herokuapp.com",
+    domainHost: "http://morning-fjord-4117.herokuapp.com",
 
-domainHost: "http://localhost:9000",
+    //domainHost: "http://localhost:9000",
     ENTER: 13,
 
            
