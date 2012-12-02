1513d596a5e3ac4a10e401bc306c969a3c7e5d37
Panu Klemola <panu.klemola@gmail.com>
2012-12-02 19:16:32 +0200

    noticet fiksattu

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,4 +1,4 @@
-/*Copyright 2012 University of Helsinki, Panu Klemola, Herkko Virolainen, Daria Antonova
+/**Copyright 2012 University of Helsinki, Daria Antonova, Herkko Virolainen, Panu Klemola
 *
 *Licensed under the Apache License, Version 2.0 (the "License");
 *you may not use this file except in compliance with the License.
